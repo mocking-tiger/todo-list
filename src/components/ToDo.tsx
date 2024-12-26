b852d1b66ba59f6bfd6558885e1a44d0169eb85a
Park JunYong <dyden119@gmail.com>
2024-12-26 23:54:27 +0900

    feat: 각 todo별 상태버튼

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,34 @@
-import { IToDo } from "../atoms";
+import { MouseEvent } from "react";
+import { IToDo, toDoState } from "../atoms";
+import { useSetRecoilState } from "recoil";
 
-export default function ToDo({ text }: IToDo) {
+export default function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+
+  const onClick = (event: MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+  };
+
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>To Do</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TODO" && (
+        <button name="TODO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
