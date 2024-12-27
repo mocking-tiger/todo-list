f469f57821329a617aff0898e150f4e8527861ca
Park JunYong <dyden119@gmail.com>
2024-12-27 19:19:48 +0900

    feat: 리스트 상태 변경 기능

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -9,8 +9,16 @@ export default function ToDo({ text, category, id }: IToDo) {
     const {
       currentTarget: { name },
     } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as IToDo["category"] };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
   };
-
   return (
     <li>
       <span>{text}</span>
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,11 +1,11 @@
 import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
 import { useRecoilValue } from "recoil";
 import { toDoState } from "../atoms";
-import ToDo from "./ToDo";
 
 export default function ToDoList() {
   const toDos = useRecoilValue(toDoState); // useState처럼 value,setValue를 한번에 불러올 수 있음
-
+  console.log(toDos);
   return (
     <div>
       <h1>To Dos</h1>
